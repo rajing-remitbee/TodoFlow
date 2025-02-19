4a852bf69d91a2912b7c01f71d19753bf8b56f73
Rajin Gangadharan <rajin.gangadharan@remitbee.com>
2025-02-19 12:35:43 +0530

    Implement completion count and conversion rate

--- TodoFlow/ViewController/StatisticsViewController.swift
+++ TodoFlow/ViewController/StatisticsViewController.swift
@@ -12,6 +12,7 @@ class StatisticsViewController: UIViewController {
     @IBOutlet var filterView: UISegmentedControl!
     
     @IBOutlet var completedLabel: UILabel!
+    @IBOutlet var completionRateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,14 +50,36 @@ class StatisticsViewController: UIViewController {
     }
     
     func updateStatistics(for filter: StatisticsFilter) {
-        let filteredTasks = filterTasks(for: filter) //Filter tasks based on segment
-        let completedTasks = filteredTasks.filter { $0.isCompleted } //Filter completed tasks
+        //Filter tasks
+        let filteredTasks = filterTasks(for: filter)
+        //Filter only completed tasks
+        let completedTasks = filteredTasks.filter { $0.isCompleted }
         
         let totalTasks = filteredTasks.count //Filter count
         let completedCount = completedTasks.count //Completed Count
         let completionRate = totalTasks > 0 ? (completedCount * 100) / totalTasks : 0 //Completion Rate
         
-        completedLabel.text = "\(completedCount)"
+        //Format task completed count text
+        let fullText = "\(completedCount)/\(totalTasks)" //Full text
+        let attributedString = NSMutableAttributedString(string: fullText) //Convert to attributed text
+        let completedRange = NSRange(location: 0, length: "\(completedCount)".count) //Calculate range of completed number
+        let completedAttributes: [NSAttributedString.Key: Any] = [ .font: UIFont.systemFont(ofSize: 36, weight: .semibold), .foregroundColor: UIColor(hex: "#0E100F")] //Apply formatting options
+        attributedString.addAttributes(completedAttributes, range: completedRange) //Add attibutes
+        let totalRange = NSRange(location: completedRange.length, length: fullText.count - completedRange.length) //Calculate range for total tasks count
+        let totalAttributes: [NSAttributedString.Key: Any] = [ .font: UIFont.systemFont(ofSize: 20, weight: .semibold), .foregroundColor: UIColor(hex: "#7E8491") ] //Apply formatting options
+        attributedString.addAttributes(totalAttributes, range: totalRange) //Add attributes
+        completedLabel.attributedText = attributedString //Set attributed text
+        
+        //Format task conversion rate text
+        let fullConversionText = "\(completionRate)%" //Full Conversion Rate Text
+        let attributedConversionString = NSMutableAttributedString(string: fullConversionText) //Convert to attributed text
+        let numberRange = NSRange(location: 0, length: "\(completionRate)".count) //Calculate range of conversion number
+        let numberAttributes: [NSAttributedString.Key: Any] = [ .font: UIFont.systemFont(ofSize: 36, weight: .semibold), .foregroundColor: UIColor(hex: "#0E100F") ] //Apply formatting options
+        attributedConversionString.addAttributes(numberAttributes, range: numberRange) //Add attributes
+        let percentRange = NSRange(location: numberRange.length, length: fullConversionText.count - numberRange.length) //Calculate range for percentage symbol
+        let percentAttributes: [NSAttributedString.Key: Any] = [ .font: UIFont.systemFont(ofSize: 20, weight: .semibold), .foregroundColor: UIColor(hex: "#7E8491") ] //Apply formatting options
+        attributedConversionString.addAttributes(percentAttributes, range: percentRange) //Add attributes
+        completionRateLabel.attributedText = attributedConversionString //Set attributed text
     }
     
     //FilterTasks Method
